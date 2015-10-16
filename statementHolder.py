5c16904d343fb74f4e8c233803e0db2d8a3e1f02
jordanCain <jordancain94@gmail.com>
2015-10-16 12:39:23 +0100

    Add basics to statementHolder

--- statementHolder.py
+++ statementHolder.py
@@ -1,4 +1,16 @@
 from methodHolder import Method
 
 class statement:
-    def __init__(self,)
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.parameters = []
+        
+    def getName(self):
+        return self.name
+        
+    def getParent(self):
+        return self.parent
+    
+    def addParameter(self, param):
+        self.parameters.add(param)
