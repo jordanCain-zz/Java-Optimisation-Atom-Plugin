4468b4d56afe6680f7f6b8e2b70e6308d818150b
jordanCain <jordancain94@gmail.com>
2015-10-19 19:31:06 +0100

    Start decision tree

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 from packageHolder import Package
 from classHolder import Class
 from methodHolder import Method
+from statementHolder import Statement
 
 javaString = 'package helloworld;\n\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}'
 #print (javaString)
@@ -12,7 +13,33 @@ newClass = Class("class", newPackage)
 print ("Get name of class: " + newClass.getName())
 print ("Get parent of class: " + newClass.getParent().getName())
 
-newMethod = Method("method", newClass, "void")
+newMethod = Method("method", newClass, "scope", "return type")
 print ("Get name of method: " + newMethod.getName())
 print ("Get parent of method: " + newMethod.getParent().getName())
 print ("Get parent of parent method: " + newMethod.getParent().getParent().getName())
+
+newStatement = Statement("statement", newMethod)
+print ("Get name of statement: " + newStatement.getName())
+print ("Get parent of statement: " + newStatement.getParent().getName())
+print ("Get parent of parent statement: " + newStatement.getParent().getParent().getName())
+print ("Get parent of parent of parent statement: " + newStatement.getParent().getParent().getParent().getName())
+
+#SOURCE:: http://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python/323259#323259
+def decision(word, currentCharPosition):
+    return {
+        'package': foundPackage("Get the package name here"),
+        #TODO:: Class usually preceeded by public/ private
+        #Must find way to get around this
+        'class': foundClass("Get the class name here"),
+        'public': foundMethod("Get the method name here and set scope public"),
+        'private': foundMethod("Get the method name here and set scope private"),
+    }.get(word, foundStatement("get statement and any params"))
+
+def foundPackage(name):
+    print ("found new package")
+
+def foundClass(name):
+    print ("Found new class")
+
+def foundMethod(name):
+    print ("Found new method")
--- statementHolder.py
+++ statementHolder.py
@@ -1,16 +1,19 @@
 from methodHolder import Method
 
-class statement:
+class Statement:
     def __init__(self, name, parent):
         self.name = name
         self.parent = parent
         self.parameters = []
-        
+
     def getName(self):
         return self.name
-        
+
     def getParent(self):
         return self.parent
-    
+
     def addParameter(self, param):
         self.parameters.add(param)
+
+    def toString(self):
+        print ("I'm a statement")
