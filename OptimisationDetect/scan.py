094aae98abae5991907ca98167c1ad7223a0e339
jordanCain <jordancain94@gmail.com>
2015-11-21 13:38:54 +0000

    Laptop Commit

--- OptimisationDetect/scan.py
+++ OptimisationDetect/scan.py
@@ -1,7 +1,11 @@
 import sys
 #SOURCE:: http://stackoverflow.com/questions/4383571/importing-files-from-different-folder-in-python
-sys.path.insert(0, r'C:\Users\jordan\Documents\GitHub\javaParser\ASTParser')
+sys.path.insert(0, r'C:\Users\Jordan\Documents\GitHub\javaParser\ASTParser')
 import parse
+#SOURCE:: http://stackoverflow.com/questions/2654113/python-how-to-get-the-callers-method-name-in-the-called-method
+import inspect
+#SOURCE:: http://stackoverflow.com/questions/1156023/print-current-call-stack-from-a-method-in-python-code
+import traceback
 from methodHolder import Method
 from classHolder import Class
 
@@ -15,6 +19,15 @@ def scan():
 #Params: parent is the highest level of the tree, debug will add extra output to console
 def recursionDetect(parent, debug):
     if debug:
+        #Stack trace experimentation
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        print ("Caller name: ", calframe[1][3])
+        #######################################
+        print ("Stack trace: ")
+        for line in traceback.format_stack():
+            print("\t" + line.strip())
+        #End of stack trace experimentation
         print ("Recursion Detection")
     #Get all the methods in the tree
     methods = getMethods(parent, debug)
@@ -25,9 +38,9 @@ def recursionDetect(parent, debug):
             if debug :
                 print("\tstatement: " + statement.getName() + " #### parent: " + statement.getParent().getName())
             if method.getName() in statement.getName():
-                if debug:
+                if True:#debug:
                     print ("found a possible recursion")
-                    print (statement.getName() + "in method: " + method.getName())
+                    print (statement.getName() + "\tin method: " + method.getName())
                 #We now need to ensure the function call is to the method we're in
                 #It could be a function with the same name but different list of Params
                 params = parse.getParams(statement.getName())
@@ -51,8 +64,8 @@ def getMethods(parent, debug):
             classMethods.append(currentClass.getChildren())
         elif debug:
             print ("\tfound not a class")
-    #Calling get parent on the top level (the package) returns a list of objects within a list
-    #If we have multiple classes in a file we get a list of lists
+    #Calling get parent on the top level (the package) returns a list of objects which we append to a list
+    #If we have multiple classes in a file we get a list with multiple lists(each list would be a class)
     #We need to filter out any objects that arent of type Method
     #SOURCE:: http://stackoverflow.com/questions/2225038/determine-the-type-of-an-object
     finalMethods = []
